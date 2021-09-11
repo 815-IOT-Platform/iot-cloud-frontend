28af1863cb40f49fbd857ad9d612544ded9adeb6
Rusty-Trueno <1076061276@qq.com>
2021-09-11 19:32:29 +0800

    update topo and analysis page

--- src/views/honeypot/Analysis.vue
+++ src/views/honeypot/Analysis.vue
@@ -1,15 +1,423 @@
 <template>
-  <div>
-    综合分析
+  <div class="page-header-index-wide">
+    <a-row :gutter="24">
+      <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '24px' }">
+        <chart-card :loading="loading" title="总蜜罐数" total="248（个）">
+          <a-tooltip title="指标说明" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <trend flag="up" style="margin-right: 16px;">
+              <span slot="term">周同比</span>
+              12%
+            </trend>
+            <trend flag="down">
+              <span slot="term">日同比</span>
+              11%
+            </trend>
+          </div>
+          <template slot="footer">日均新增蜜罐数<span>10.5（个）</span></template>
+        </chart-card>
+      </a-col>
+      <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '24px' }">
+        <chart-card :loading="loading" title="蜜罐访问量" :total="8846 | NumberFormat">
+          <a-tooltip title="指标说明" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <mini-area />
+          </div>
+          <template slot="footer">日访问量<span> {{ '34' | NumberFormat }}</span></template>
+        </chart-card>
+      </a-col>
+      <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '24px' }">
+        <chart-card :loading="loading" title="工作蜜罐数" :total="210 | NumberFormat">
+          <a-tooltip title="指标说明" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <mini-bar />
+          </div>
+          <template slot="footer">工作率 <span>60%</span></template>
+        </chart-card>
+      </a-col>
+      <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '24px' }">
+        <chart-card :loading="loading" title="触发蜜罐占比" total="78%">
+          <a-tooltip title="指标说明" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <mini-progress color="rgb(19, 194, 194)" :target="80" :percentage="78" height="8px" />
+          </div>
+          <template slot="footer">
+            <trend flag="down" style="margin-right: 16px;">
+              <span slot="term">同周比</span>
+              12%
+            </trend>
+            <trend flag="up">
+              <span slot="term">日环比</span>
+              80%
+            </trend>
+          </template>
+        </chart-card>
+      </a-col>
+    </a-row>
+
+    <a-card :loading="loading" :bordered="false" :body-style="{padding: '0'}">
+      <div class="salesCard">
+        <a-tabs default-active-key="1" size="large" :tab-bar-style="{marginBottom: '24px', paddingLeft: '16px'}">
+          <div class="extra-wrapper" slot="tabBarExtraContent">
+            <div class="extra-item">
+              <a>今日</a>
+              <a>本周</a>
+              <a>本月</a>
+              <a>本年</a>
+            </div>
+            <a-range-picker :style="{width: '256px'}" />
+          </div>
+          <a-tab-pane loading="true" tab="蜜罐数" key="1">
+            <a-row>
+              <a-col :xl="16" :lg="12" :md="12" :sm="24" :xs="24">
+                <bar :data="barData" title="蜜罐数排行" />
+              </a-col>
+              <a-col :xl="8" :lg="12" :md="12" :sm="24" :xs="24">
+                <rank-list title="节点蜜罐数排行榜" :list="rankListEdge"/>
+              </a-col>
+            </a-row>
+          </a-tab-pane>
+          <a-tab-pane tab="触发量" key="2">
+            <a-row>
+              <a-col :xl="16" :lg="12" :md="12" :sm="24" :xs="24">
+                <bar :data="barData2" title="触发量趋势" />
+              </a-col>
+              <a-col :xl="8" :lg="12" :md="12" :sm="24" :xs="24">
+                <rank-list title="触发量排行榜" :list="rankListPot"/>
+              </a-col>
+            </a-row>
+          </a-tab-pane>
+        </a-tabs>
+      </div>
+    </a-card>
+
+    <div class="antd-pro-pages-dashboard-analysis-twoColLayout" :class="isDesktop() ? 'desktop' : ''">
+      <a-row :gutter="24">
+        <a-col :xl="12" :lg="24" :md="24" :sm="24" :xs="24">
+          <a-card :loading="loading" :bordered="false" title="热门蜜罐攻击" :style="{ marginTop: '24px', minHeight: '500px' }">
+            <a-dropdown :trigger="['click']" placement="bottomLeft" slot="extra">
+              <a class="ant-dropdown-link" href="#">
+                <a-icon type="ellipsis" />
+              </a>
+              <a-menu slot="overlay">
+                <a-menu-item>
+                  <a href="javascript:;">操作一</a>
+                </a-menu-item>
+                <a-menu-item>
+                  <a href="javascript:;">操作二</a>
+                </a-menu-item>
+              </a-menu>
+            </a-dropdown>
+            <a-row :gutter="68">
+              <a-col :xs="24" :sm="12" :style="{ marginBottom: ' 24px'}">
+                <number-info :total="12321" :sub-total="17.1">
+                  <span slot="subtitle">
+                    <span>蜜罐被攻击总数</span>
+                    <a-tooltip title="指标说明" slot="action">
+                      <a-icon type="info-circle-o" :style="{ marginLeft: '8px' }" />
+                    </a-tooltip>
+                  </span>
+                </number-info>
+                <!-- miniChart -->
+                <div>
+                  <mini-smooth-area :style="{ height: '45px' }" :dataSource="searchUserData" :scale="searchUserScale" />
+                </div>
+              </a-col>
+              <a-col :xs="24" :sm="12" :style="{ marginBottom: ' 24px'}">
+                <number-info :total="2.7" :sub-total="26.2" status="down">
+                  <span slot="subtitle">
+                    <span>平均蜜罐被攻击数</span>
+                    <a-tooltip title="指标说明" slot="action">
+                      <a-icon type="info-circle-o" :style="{ marginLeft: '8px' }" />
+                    </a-tooltip>
+                  </span>
+                </number-info>
+                <!-- miniChart -->
+                <div>
+                  <mini-smooth-area :style="{ height: '45px' }" :dataSource="searchUserData" :scale="searchUserScale" />
+                </div>
+              </a-col>
+            </a-row>
+            <div class="ant-table-wrapper">
+              <a-table
+                row-key="index"
+                size="small"
+                :columns="searchTableColumns"
+                :dataSource="searchData"
+                :pagination="{ pageSize: 5 }"
+              >
+                <span slot="range" slot-scope="text, record">
+                  <trend :flag="record.status === 0 ? 'up' : 'down'">
+                    {{ text }}%
+                  </trend>
+                </span>
+              </a-table>
+            </div>
+          </a-card>
+        </a-col>
+        <a-col :xl="12" :lg="24" :md="24" :sm="24" :xs="24">
+          <a-card class="antd-pro-pages-dashboard-analysis-salesCard" :loading="loading" :bordered="false" title="交互类别攻击占比" :style="{ marginTop: '24px', minHeight: '500px' }">
+            <div slot="extra" style="height: inherit;">
+              <!-- style="bottom: 12px;display: inline-block;" -->
+              <span class="dashboard-analysis-iconGroup">
+                <a-dropdown :trigger="['click']" placement="bottomLeft">
+                  <a-icon type="ellipsis" class="ant-dropdown-link" />
+                  <a-menu slot="overlay">
+                    <a-menu-item>
+                      <a href="javascript:;">操作一</a>
+                    </a-menu-item>
+                    <a-menu-item>
+                      <a href="javascript:;">操作二</a>
+                    </a-menu-item>
+                  </a-menu>
+                </a-dropdown>
+              </span>
+              <div class="analysis-salesTypeRadio">
+                <a-radio-group defaultValue="a">
+                  <a-radio-button value="a">全部</a-radio-button>
+                  <a-radio-button value="b">高交互</a-radio-button>
+                  <a-radio-button value="c">中低交互</a-radio-button>
+                </a-radio-group>
+              </div>
+
+            </div>
+            <h4>被攻击数</h4>
+            <div>
+              <!-- style="width: calc(100% - 240px);" -->
+              <div>
+                <v-chart :force-fit="true" :height="405" :data="pieData" :scale="pieScale">
+                  <v-tooltip :showTitle="false" dataKey="item*percent" />
+                  <v-axis />
+                  <!-- position="right" :offsetX="-140" -->
+                  <v-legend dataKey="item"/>
+                  <v-pie position="percent" color="item" :vStyle="pieStyle" />
+                  <v-coord type="theta" :radius="0.75" :innerRadius="0.6" />
+                </v-chart>
+              </div>
+
+            </div>
+          </a-card>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+import { ChartCard, MiniArea, MiniBar, MiniProgress, RankList, Bar, Trend, NumberInfo, MiniSmoothArea } from '@/components'
+import { mixinDevice } from '@/utils/mixin'
+
+const barData = []
+const barData2 = []
+for (let i = 0; i < 12; i += 1) {
+  barData.push({
+    x: `${i + 1}月`,
+    y: Math.floor(Math.random() * 1000) + 200
+  })
+  barData2.push({
+    x: `${i + 1}月`,
+    y: Math.floor(Math.random() * 1000) + 200
+  })
+}
+
+const rankListEdge = []
+for (let i = 0; i < 7; i++) {
+  rankListEdge.push({
+    name: 'edge ' + (i + 1),
+    total: 123 - i * 10
+  })
+}
+
+const rankListPot = []
+const potType = ['redis', 'http', 'ssh', 'telnet', 'https', 'docker', 'ftp']
+for (let i = 0; i < 7; i++) {
+  rankListPot.push({
+    name: potType[i],
+    total: 3746 - i * 100
+  })
+}
+
+const searchUserData = []
+for (let i = 0; i < 7; i++) {
+  searchUserData.push({
+    x: moment().add(i, 'days').format('YYYY-MM-DD'),
+    y: Math.ceil(Math.random() * 10)
+  })
+}
+const searchUserScale = [
+  {
+    dataKey: 'x',
+    alias: '时间'
+  },
+  {
+    dataKey: 'y',
+    alias: '用户数',
+    min: 0,
+    max: 10
+  }]
+
+const searchTableColumns = [
+  {
+    dataIndex: 'index',
+    title: '排名',
+    width: 90
+  },
+  {
+    dataIndex: 'keyword',
+    title: '蜜罐类型'
+  },
+  {
+    dataIndex: 'count',
+    title: '被攻击数'
+  },
+  {
+    dataIndex: 'range',
+    title: '周涨幅',
+    align: 'right',
+    sorter: (a, b) => a.range - b.range,
+    scopedSlots: { customRender: 'range' }
+  }
+]
+const searchData = []
+for (let i = 0; i < 50; i += 1) {
+  searchData.push({
+    index: i + 1,
+    keyword: potType[i % 7],
+    count: Math.floor(Math.random() * 1000),
+    range: Math.floor(Math.random() * 100),
+    status: Math.floor((Math.random() * 10) % 2)
+  })
+}
+
+const DataSet = require('@antv/data-set')
+
+const sourceData = [
+  { item: 'redis', count: 32.2 },
+  { item: 'http', count: 21 },
+  { item: 'ssh', count: 17 },
+  { item: 'telnet', count: 13 },
+  { item: 'https', count: 9 },
+  { item: '其他', count: 7.8 }
+]
+
+const pieScale = [{
+  dataKey: 'percent',
+  min: 0,
+  formatter: '.0%'
+}]
+
+const dv = new DataSet.View().source(sourceData)
+dv.transform({
+  type: 'percent',
+  field: 'count',
+  dimension: 'item',
+  as: 'percent'
+})
+const pieData = dv.rows
+
 export default {
-  name: 'Analysis'
+  name: 'PotAnalysis',
+  mixins: [mixinDevice],
+  components: {
+    ChartCard,
+    MiniArea,
+    MiniBar,
+    MiniProgress,
+    RankList,
+    Bar,
+    Trend,
+    NumberInfo,
+    MiniSmoothArea
+  },
+  data () {
+    return {
+      loading: true,
+      rankListEdge,
+      rankListPot,
+
+      // 搜索用户数
+      searchUserData,
+      searchUserScale,
+      searchTableColumns,
+      searchData,
+
+      barData,
+      barData2,
+
+      //
+      pieScale,
+      pieData,
+      sourceData,
+      pieStyle: {
+        stroke: '#fff',
+        lineWidth: 1
+      }
+    }
+  },
+  created () {
+    setTimeout(() => {
+      this.loading = !this.loading
+    }, 1000)
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.extra-wrapper {
+  line-height: 55px;
+  padding-right: 24px;
 
+.extra-item {
+  display: inline-block;
+  margin-right: 24px;
+
+a {
+  margin-left: 24px;
+}
+}
+}
+
+.antd-pro-pages-dashboard-analysis-twoColLayout {
+  position: relative;
+  display: flex;
+  display: block;
+  flex-flow: row wrap;
+
+&.desktop div[class^=ant-col]:last-child {
+   position: absolute;
+   right: 0;
+   height: 100%;
+ }
+}
+
+.antd-pro-pages-dashboard-analysis-salesCard {
+  height: calc(100% - 24px);
+/deep/ .ant-card-head {
+  position: relative;
+}
+}
+
+.dashboard-analysis-iconGroup {
+i {
+  margin-left: 16px;
+  color: rgba(0,0,0,.45);
+  cursor: pointer;
+  transition: color .32s;
+  color: black;
+}
+}
+.analysis-salesTypeRadio {
+  position: absolute;
+  right: 54px;
+  bottom: 12px;
+}
 </style>
--- src/views/honeypot/Visualization.vue
+++ src/views/honeypot/Visualization.vue
@@ -1,78 +1,120 @@
 <template>
   <div>
-    <dv-flyline-chart-enhanced class="flyline-chart" :config="config" dev style="width:100%;height:100%;" />
+    <a-card title="全局拓扑">
+      <network
+        class="network"
+        :nodes="network.nodes"
+        :edges="network.edges"
+        :options="network.options"
+      ></network>
+    </a-card>
   </div>
 </template>
 
 <script>
+import { Network } from 'vue-vis-network'
+import Host from './static/hosting.png'
+import Pot from './static/pot.png'
+import master from './static/k8s.png'
+import { getAllEdgeNode, getPotList } from '@/api/honeypot'
 
 export default {
-  name: 'visualization',
-  data () {
+  name: 'Visualization',
+  components: {
+    Network
+  },
+  data() {
     return {
-      config: {
-        icon: {
-          show: true,
-          // src: require('../assets/img/zuobiao.png'),
-          width: 50,
-          height: 50
-        },
-        points: [
-          {
-            name: '内蒙古',
-            coordinate: [0.59, 0.37],
-            icon: {
-              show: true,
-              // src: require('../assets/img/zuobiao-center.png'),
-              width: 50,
-              height: 50
+      network: {
+        nodes: [],
+        edges: [],
+        options: {
+          nodes: {
+            shape: 'circle',
+            size: 24,
+            color: {
+              border: 'grey',
+
+              highlight: {
+                border: 'black',
+                background: 'white'
+              },
+              hover: {
+                border: 'orange',
+                background: 'grey'
+              }
             },
-            halo: {
-              show: true,
-            },
-          },
-          {
-            name: '青海省',
-            coordinate: [0.36, 0.52]
-          },
-          {
-            name: '四川省',
-            coordinate: [0.48, 0.66]
-          },
-          {
-            name: '北京市',
-            coordinate: [0.68, 0.35]
-          },
-        ],
-        lines: [
-          {
-            source: '内蒙古',
-            target: '青海省'
-          },
-          {
-            source: '内蒙古',
-            target: '四川省',
-            color: '#00feff'
-          },
-          {
-            source: '内蒙古',
-            target: '北京市'
-          },
-        ],
-        line: {
-          width: 2
-        },
-      },
+            font: { color: 'black' },
+            shapeProperties: {
+              useBorderWithImage: false
+            }
+          }
+        }
+      }
     }
   },
-  beforeCreate () {
-
+  methods: {
+    initTopology() {
+      this.network.nodes.push({
+        id: 1,
+        label: 'master',
+        shape: 'image',
+        image: master
+      })
+      let that = this
+      let nodeCnt = 2
+      let edgeCnt = 0
+      getAllEdgeNode().then(res => {
+        res.data.map(function (node) {
+          that.network.nodes.push({
+            id: nodeCnt,
+            label: node.name,
+            shape: 'image',
+            image: Host
+          })
+          that.network.edges.push({
+            id: edgeCnt,
+            from: nodeCnt,
+            to: 1
+          })
+          let edgeNode = nodeCnt
+          edgeCnt++
+          nodeCnt++
+          getPotList(node.name).then(res => {
+            res.data.map(function (pot) {
+              that.network.nodes.push({
+                id: nodeCnt,
+                label: '蜜罐名：' + pot.name + '\n协议：' + pot.protocol + '\n被攻击数: ' + pot.attackCnt + '\n端口：' + pot.port,
+                shape: 'image',
+                image: Pot,
+                smooth: true
+              })
+              that.network.edges.push({
+                id: edgeCnt,
+                from: nodeCnt,
+                to: edgeNode
+              })
+              edgeCnt++
+              nodeCnt++
+            })
+          })
+        })
+      })
+    }
   },
   created () {
-
+    this.initTopology()
   },
-  methods: {
-
-  }
 }
 </script>
+
+<style scoped>
+
+.network {
+  height: 600px;
+  border: 1px solid #ccc;
+  margin: 5px 0;
+}
+
+</style>
+
