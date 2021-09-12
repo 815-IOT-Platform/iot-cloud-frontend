0eac84a0e330584628a60859899312da8e145297
Rusty-Trueno <1076061276@qq.com>
2021-09-12 18:56:31 +0800

    add pot forward page

---
+++ src/views/honeypot/PotForward.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <iframe src="http://127.0.0.1:8080/u/main" id="mobsf" scrolling="no" frameborder="0" style="left: 240px; height: 1000px; right:0px;bottom:100px;"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PotForward',
+  mounted () {
+    /**
+     * iframe-宽高自适应显示
+     */
+    function changeMobsfIframe () {
+      const mobsf = document.getElementById('mobsf');
+      const deviceWidth = document.body.clientWidth;
+      const deviceHeight = document.body.clientHeight;
+      mobsf.style.width = (Number(deviceWidth)-240) + 'px';
+      mobsf.style.height = (Number(deviceHeight)-64) + 'px';
+    }
+
+    changeMobsfIframe()
+
+    window.onresize = function () {
+      changeMobsfIframe()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
